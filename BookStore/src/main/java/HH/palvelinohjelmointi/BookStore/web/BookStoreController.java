ec0883b7bcfa50094065c112bc721e68029288d2
ErikLin100 <bgr@myy.haaga-helia.fi>
2022-02-10 00:30:13 +0200

    ekat lisäykset

---
+++ BookStore/src/main/java/HH/palvelinohjelmointi/BookStore/web/BookStoreController.java
@@ -0,0 +1,27 @@
+package HH.palvelinohjelmointi.BookStore.web;
+
+import HH.palvelinohjelmointi.BookStore.domain.Book;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class BookStoreController {
+	
+	
+		@RequestMapping(value = "/index", method = RequestMethod.GET)
+		public String getBooks(Model model) {
+			List<Book> Books = new ArrayList<Book>(); 
+			model.addAttribute("Books", Books);
+			return "booklist"; 
+								
+		}
+
+	
+
+}
