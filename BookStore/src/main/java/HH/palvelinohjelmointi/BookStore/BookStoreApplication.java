d2c9e7e5fdbd64261f468c2fe63b821615dac1b6
ErikLin100 <bgr@myy.haaga-helia.fi>
2022-02-17 11:56:36 +0200

    css lisäykset

--- BookStore/src/main/java/HH/palvelinohjelmointi/BookStore/BookStoreApplication.java
+++ BookStore/src/main/java/HH/palvelinohjelmointi/BookStore/BookStoreApplication.java
@@ -1,31 +1,47 @@
 package HH.palvelinohjelmointi.BookStore;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 
+
 import HH.palvelinohjelmointi.BookStore.domain.Book;
 import HH.palvelinohjelmointi.BookStore.domain.BookRepository;
 
+
+
 @SpringBootApplication
 public class BookStoreApplication {
+	private static final Logger log = LoggerFactory.getLogger(BookStoreApplication.class); 
 
 	public static void main(String[] args) {
 		SpringApplication.run(BookStoreApplication.class, args);
 	}
+
+	
 	
 	@Bean
-	public CommandLineRunner demo(BookRepository repository) 
+	public CommandLineRunner demo(BookRepository bookRepository) 
 	{return (args) -> {
 	
 		Book b1 = new Book("Breathe: A Life in Flow", "Rickson Gracie", 2021,"978-0-06-301895-2", 27.99);
 		Book b2 = new Book("Naked economics", "Charles Wheelan", 2002,"978-0-393-35649-6", 16.95);
 		
-		repository.save(b1);
-		repository.save(b2);
+		bookRepository.save(b1);
+		bookRepository.save(b2);
+		
+		
+		log.info("fetch all books");
+		for (Book book : bookRepository.findAll()) {
+			log.info(book.toString());
+		}
+
 	};
+	
 	
 
 	}
--- BookStore/src/main/java/HH/palvelinohjelmointi/BookStore/web/BookStoreController.java
+++ BookStore/src/main/java/HH/palvelinohjelmointi/BookStore/web/BookStoreController.java
@@ -1,28 +1,53 @@
 package HH.palvelinohjelmointi.BookStore.web;
 
 import HH.palvelinohjelmointi.BookStore.domain.Book;
+import HH.palvelinohjelmointi.BookStore.domain.BookRepository;
 
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class BookStoreController {
 	
+	@Autowired
+	BookRepository bookRepository; 
 	
-		@RequestMapping(value = "/index", method = RequestMethod.GET)
-		public String getBooks(Model model) {
-			List<Book> Books = new ArrayList<Book>();
-			model.addAttribute("Books", Books);
+	
+	@RequestMapping(value = "/booklist", method = RequestMethod.GET)
+	public String getBooks(Model model) {
+			List<Book> book =  (List<Book>) bookRepository.findAll(); 
+			model.addAttribute("book", book); 
 			return "booklist"; 
 								
 		}
+	@RequestMapping(value = "/newbook", method = RequestMethod.GET)
+	public String getaddNewBook(Model model) {
+		model.addAttribute("book", new Book());
+		return "addbook";
+	}
 
+	
+	@RequestMapping(value = "/savebook", method = RequestMethod.POST)
+	public String saveBook(@ModelAttribute Book book) {
+		
+		bookRepository.save(book);
+		return "redirect:/booklist";
+	}
+
+	
+	@RequestMapping(value = "/deletebook/{id}", method = RequestMethod.GET)
+	public String deleteBook(@PathVariable("id") Long bookId) {
+		bookRepository.deleteById(bookId);
+		return "redirect:../booklist";
+	}
 	
 
 }
